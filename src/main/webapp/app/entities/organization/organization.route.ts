31ba4ca8caaa28f09161e70ba8f396001fcb5c29
rodrik <rodrigo.ijk@gmail.com>
2017-10-02 16:36:40 +1300

    Secure organization administration only to ROLE_ADMIN

--- src/main/webapp/app/entities/organization/organization.route.ts
+++ src/main/webapp/app/entities/organization/organization.route.ts
@@ -14,7 +14,7 @@ export const organizationRoute: Routes = [
         path: 'organization',
         component: OrganizationComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'Organizations'
         },
         canActivate: [UserRouteAccessService]
@@ -22,7 +22,7 @@ export const organizationRoute: Routes = [
         path: 'organization/:id',
         component: OrganizationDetailComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'Organizations'
         },
         canActivate: [UserRouteAccessService]
@@ -34,7 +34,7 @@ export const organizationPopupRoute: Routes = [
         path: 'organization-new',
         component: OrganizationPopupComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'Organizations'
         },
         canActivate: [UserRouteAccessService],
@@ -44,7 +44,7 @@ export const organizationPopupRoute: Routes = [
         path: 'organization/:id/edit',
         component: OrganizationPopupComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'Organizations'
         },
         canActivate: [UserRouteAccessService],
@@ -54,7 +54,7 @@ export const organizationPopupRoute: Routes = [
         path: 'organization/:id/delete',
         component: OrganizationDeletePopupComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'Organizations'
         },
         canActivate: [UserRouteAccessService],
